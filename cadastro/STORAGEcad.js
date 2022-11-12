4f9d0a9e2569d3bdb8ec1e67e1c72ab2f8f7cf8e
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2022-11-12 20:34:52 -0300

    Create STORAGEcad.js

---
+++ cadastro/STORAGEcad.js
@@ -0,0 +1,5 @@
+function RecarregarSTCAD() {
+ localStorage.removeItem('listaUser')
+console.warn("STORAGECAD> Contas UNIVERSAL e ADMINISTRATIVAS Carregadas")
+alert("O Banco de Dados Foi Recarregado")
+}
