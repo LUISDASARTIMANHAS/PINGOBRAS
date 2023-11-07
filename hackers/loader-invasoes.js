fcaa7c94e0e8f239b4936a99517e1eb528b6db4f
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-11-07 00:02:18 -0300

    updated

---
+++ hackers/loader-invasoes.js
@@ -0,0 +1,32 @@
+
+
+function getData() {
+    const url = "https://pingobras-sg.glitch.me/api/thegame/hackers/invasions";
+    const options = {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "content-type": "application/json;charset=utf-8",
+            Authorization: "snve072509ç$",
+        }
+    };
+
+    fetch(url, options)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Erro na solicitação, URL inválida ou fetch inválido");
+                return response.text();
+            }
+        })
+        .then((data) => {
+            console.log("DATA RESPONSE: ");
+            console.log(data);
+        })
+        .catch((error) => errosLogin(error));
+}
+
+function errosLogin(error) {
+    console.debug(error);
+}
