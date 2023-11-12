dc65d582131882fb52551fb294a8522d097aa993
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-11-12 00:47:47 -0300

    updated

--- hackers/loader-invasoes.js
+++ hackers/loader-invasoes.js
@@ -21,63 +21,52 @@ fetch(url, options)
     .then((data) => {
         console.log("DATA RESPONSE: ");
         console.log(data);
+        criarTabela(data);
     })
     .catch((error) => errosLogin(error));
 
 
 function errosLogin(error) {
     console.debug(error);
-    alert(error);
 }
 
 
-function criarTabelaEdital(data) {
-    for (let i = 0; i < data.length; i++) {
-        const edital = data[i];
-        const editais = edital.edital;
-        const descricao = edital.descricao;
-        const dataDeInicio = edital.dataDeInicio;
-        const dataDeTermino = edital.dataDeTermino;
-        const status = edital.status;
-        const inscritos = data[i].inscritos
-        console.log(inscritos)
+function criarTabela(database) {
+    for (let i = 0; i < database.length; i++) {
+        const invasion = database[i];
+        const base = invasion.base;
+        const bitcoins = invasion.bitcoins;
+        const money = invasion.money;
+        const reputacao = invasion.reputacao;
+        const replayLink = edital.replayLink;
 
-        if (status == "Aberto" || status == "Fechado") {
             var trLine = document.createElement("tr");
             var aElement = document.createElement("a");
-            var tdElementEditais = document.createElement("td");
-            var tdElementDescricao = document.createElement("td");
-            var tdElementDataIni = document.createElement("td");
-            var tdElementDataTerm = document.createElement("td");
-            var tdElementStatus = document.createElement("td");
+            var tdElementBase = document.createElement("td");
+            var tdElementBiticoins = document.createElement("td");
+            var tdElementMoney = document.createElement("td");
+            var tdElementReputacao = document.createElement("td");
+            var tdElementReplay = document.createElement("td");
             var tdElementAcoes = document.createElement("td");
 
-            if (status == "Aberto") {
-                aElement.innerHTML = "Inscrever Se";
-                aElement.setAttribute("onclick", "inscreverSe(" + "'" + editais + "'" + ")");
-                aElement.href = "#";
-            } else {
-                aElement.innerHTML = "Ver";
-                aElement.href = "edital/results.html";
-            }
+            aElement.innerHTML = "View Replay";
+            aElement.href = "edital/results.html";
 
-            tdElementEditais.innerHTML = editais;
-            tdElementDescricao.innerHTML = descricao;
-            tdElementDescricao.classList.add("info");
-            tdElementDataIni.innerHTML = dataDeInicio;
-            tdElementDataTerm.innerHTML = dataDeTermino;
-            tdElementStatus.innerHTML = status;
+            tdElementBase.innerHTML = base;
+            tdElementBiticoins.innerHTML = bitcoins;
+            tdElementMoney.innerHTML = money;
+            tdElementReputacao.innerHTML = reputacao;
+            tdElementReplay.innerHTML = replayLink;
             tdElementAcoes.classList.add("acoes");
             tdElementAcoes.appendChild(aElement);
 
-            trLine.appendChild(tdElementEditais);
-            trLine.appendChild(tdElementDescricao);
-            trLine.appendChild(tdElementDataIni);
-            trLine.appendChild(tdElementDataTerm);
-            trLine.appendChild(tdElementStatus);
+            trLine.appendChild(tdElementBase);
+            trLine.appendChild(tdElementBiticoins);
+            trLine.appendChild(tdElementMoney);
+            trLine.appendChild(tdElementReputacao);
+            trLine.appendChild(tdElementReplay);
             trLine.appendChild(tdElementAcoes);
 
             tabela.appendChild(trLine);
-        }
     }
 }
