c3d662d8516baeb31d9e319d7deec1f47545fce5
LUIS_DAS_ARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-07-05 09:19:26 -0300

    Update manutencao-redirect.js

--- src/js/manutencao-redirect.js
+++ src/js/manutencao-redirect.js
@@ -1,17 +1,52 @@
-(() => {
-  const body = document.querySelector("body");
-  const offline = true;
-  const authDomain = window.location.hostname == "pingobras.glitch.me";
+import config from "./config.js";
+window.addEventListener("load", () => {
+  try {
+    const url = `${config.apiUrl}/manutencao`;
+    const options = {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "content-type": "application/json;charset=utf-8",
+      },
+    };
+    fetch(url, options)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.text().then((errorText) => {
+            const errorMessage = `Statuscode: ${response.status} - ${errorText}`;
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        redirectManutencao(data);
+      })
+      .catch((error) => {
+        console.debug(`%c [SISTEMA MANUTENÇÃO] ${error}`, "color: #ff0000");
+      });
 
-  if (offline && authDomain) {
-    body.hidden = true;
+    function redirectManutencao(offline) {
+      const devUser = JSON.parse(localStorage.getItem("dev")) || false;
+      const expUser =
+        JSON.parse(localStorage.getItem("experimentalMode")) || false;
+      const body = document.querySelector("body");
 
-    if (body) {
-      body.style.display = "none";
+      if ((offline && !devUser) || (offline && !devUser)) {
+        body.hidden = true;
+
+        if (body) {
+          body.style.display = "none";
+        }
+
+        setTimeout(() => {
+          window.location.href = "./sys/manutencao.html";
+        }, 3000);
+      }
     }
-
-    setTimeout(() => {
-      window.location.href = "/sys/manutencao.html";
-    }, 5000);
+    redirectManutencao(true);
+  } catch (error) {
+    alert(`ERRO FATAL: ${error}`);
   }
-})();
+});
