c61a0b63b2a4751fcc8b20c39016b966346f4346
LUIS_DAS_ARTIMANHAS <luisaugustodesouza785@gmail.com>
2026-08-12 16:56:57 -0300

    default config add

---
+++ src/globalScritps/config.js
@@ -0,0 +1,6 @@
+export default {
+	serverUrl: "https://pingobras-factorio-server.onrender.com",
+	apiUrl: "https://pingobras-factorio-server.onrender.com/api",
+	ipToken: "f1dd48b5ecd676",
+	defaultTimeoutSeconds: 5,
+};
